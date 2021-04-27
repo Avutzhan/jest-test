f5d540524586a0056c0f24fdba446bda7a2b585a
Avutzhan Dautov <dautov92@list.ru>
2021-04-27 16:14:22 +0600

    tests

---
+++ script.test.js
@@ -0,0 +1,34 @@
+const googleSearch = require('./script');
+
+dbMock = [
+    'dog.com',
+    'cheesepuff.com',
+    'disney.com',
+    'disney2.com'
+];
+
+describe('googleSearch', () => {
+    it('is a silly test', () => {
+        expect('hello').toBe('hello');
+
+    })
+
+    it('is searching google', () => {
+        expect(googleSearch('testes', dbMock)).toEqual([]);
+        expect(googleSearch('dog', dbMock)).toEqual(['dog.com']);
+    })
+
+    it('work with undefined and null input', () => {
+        expect(googleSearch(undefined, dbMock)).toEqual([]);
+        expect(googleSearch(null, dbMock)).toEqual([]);
+
+    })
+
+    it('does not return more than 3 matches', () => {
+        expect(googleSearch('.com', dbMock).length).toEqual(3);
+
+
+    })
+
+})
+
